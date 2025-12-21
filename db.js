54af297bb2f337443b86ae93b1cf6787ff32a702
nicovon24 <nicovon24@gmail.com>
2025-12-21 17:02:34 -0300

    configurar db para supabase

--- db.js
+++ db.js
@@ -2,7 +2,23 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialect: "postgres",
 	logging: false, //hidding logs in console
+	dialectOptions: {
+		ssl: isProduction
+			? {
+				require: true,
+				rejectUnauthorized: false, // Necesario para Supabase
+			}
+			: false,
+	},
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000,
+	},
 });
